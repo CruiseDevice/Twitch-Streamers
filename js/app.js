d5f8a2909a2d230afd99ba8cefd3e23a447b6bd0
Akash Chavan <meakashchavan@gmail.com>
2017-01-30 23:10:53 +0530

    success getting data from twitch-api. next step will be to parse it and display on web page. There is so much work to do.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 console.log('working');
+var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+streamers.forEach(function(streamer){
+  ajax(streamer);
+});
+function ajax(streamer){
+  console.log(streamer);
+  url = "https://wind-bow.gomix.me/twitch-api/channels/" + streamer + "?callback=?",
+  $.ajax({
+    url: url,
+    type: 'GET',
+    dataType: 'jsonp',
+    data: {
+      action: 'query',
+      format:'json'
+    }
+  })
+  .done(function(response) {
+    console.log(url);
+    console.log(response);
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+
+
+}
