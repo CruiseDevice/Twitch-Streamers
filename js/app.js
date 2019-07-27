f4ebb9f4241f093b9bb18e11eb74b64931594645
CruiseDevice <meakashchavan@gmail.com>
2019-07-27 17:05:18 +0530

    Display response links on the html page.

--- js/app.js
+++ js/app.js
@@ -1,61 +1,57 @@
-let clientID = config.Client_ID;
-let $streamers = $(".streamers-list");
-let streamers = ["ESL_SC2", "ESL_CSGO", "freecodecamp", "storbeck",
-                "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger",
-                "noobs2ninjas", "beohoff", "brunofin", "comster404",
-                "test_channel", "cretetion", "sheevergaming", "TR7K",
-                "OgamingSC2", "monstercat", "pink_sparkles"];
-let x = 0;
+$(document).ready(function(){
+  let clientID = config.Client_ID;
+  let $streamers = $(".streamers-list");
+  let streamers = ["ESL_SC2", "ESL_CSGO", "freecodecamp", "storbeck",
+                  "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger",
+                  "noobs2ninjas", "beohoff", "brunofin", "comster404",
+                  "test_channel", "cretetion", "sheevergaming", "TR7K",
+                  "OgamingSC2", "monstercat", "pink_sparkles"];
 
-let baseURL = "https://api.twitch.tv/kraken";
-streamers.forEach(function(streamer){
-  ajax(streamer);
+  let x = 0;
+  let baseURL = "https://api.twitch.tv/kraken";
+  let api_streamers = [];
+
+  streamers.forEach(function(streamer, index){
+    ajax(streamer, index);
+  });
+
+
+  function prepareURL(type, name){
+      return baseURL + "/" + type + "/" + name;
+  }
+
+
+  function ajax(streamer, index){
+    $.ajax({
+      url: prepareURL("streams", streamer),
+      type: 'GET',
+      dataType: 'json',
+      headers: {
+          'Client-ID': clientID,
+      },
+      success: function(data){
+        api_streamers[index] = data;
+      },
+      error: function(error){
+          console.log(error);
+      }
+    })
+  };
+
+  let all = $('#all-users');
+
+  let all_section = $('.online');
+
+  console.log(all_section);
+  all.on('click', function(){
+    showResponse();
+  })
+
+  function showResponse(){
+    api_streamers.forEach(function(streamer){
+      console.log(streamer._links.channel);
+      all_section.append(streamer._links.channel + '\n');
+    });
+  }
 });
 
-
-function prepareURL(type, name){
-    return baseURL + "/" + type + "/" + name;
-}
-
-function ajax(streamer){
-  $.ajax({
-    url: prepareURL("streams", streamer),
-    type: 'GET',
-    dataType: 'json',
-    headers: {
-        'Client-ID': clientID,
-    },
-    success: function(data){
-        console.log(data);
-    },
-    error: function(error){
-        console.log(error);
-    }
-  })
-}
-function showResponse(response){
-  if(response.stream === null){
-
-  }else if(response.status == 422 || response.status == 404){
-
-  }else{
-    if(response.stream.channel.logo !== null){
-    }else{
-
-    }
-  }
-}
-function updateOfflineUsers(){
-  $.ajax({
-    url: 'https://wind-bow.gomix.me/twitch-api/channels/'+url,
-    type: 'GET',
-    dataType: 'jsonp',
-    data: {format: 'json'}
-  })
-  .done(function(response) {
-
-  })
-  .fail(function() {
-
-  })
-}
