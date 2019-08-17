a6bebc6db3ab37e9eb30fa6799c0e707b571aa5e
CruiseDevice <meakashchavan@gmail.com>
2019-08-18 01:37:54 +0530

    Show All, Online, Offline results on button click

--- js/app.js
+++ js/app.js
@@ -1,57 +1,83 @@
 $(document).ready(function(){
-  let clientID = config.Client_ID;
-  let $streamers = $(".streamers-list");
-  let streamers = ["ESL_SC2", "ESL_CSGO", "freecodecamp", "storbeck",
-                  "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger",
-                  "noobs2ninjas", "beohoff", "brunofin", "comster404",
-                  "test_channel", "cretetion", "sheevergaming", "TR7K",
-                  "OgamingSC2", "monstercat", "pink_sparkles"];
+    let clientID = config.Client_ID;
+    let $streamers = $(".streamers-list");
+    let streamers = ["ESL_SC2", "ESL_CSGO", "freecodecamp", "storbeck",
+                    "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger",
+                    "noobs2ninjas", "beohoff", "brunofin", "comster404",
+                    "test_channel", "cretetion", "sheevergaming", "TR7K",
+                    "OgamingSC2", "monstercat", "pink_sparkles"];
 
-  let x = 0;
-  let baseURL = "https://api.twitch.tv/kraken";
-  let api_streamers = [];
+    let x = 0;
+    let baseURL = "https://api.twitch.tv/kraken";
+    let api_streamers = [];
 
-  streamers.forEach(function(streamer, index){
-    ajax(streamer, index);
-  });
-
-
-  function prepareURL(type, name){
-      return baseURL + "/" + type + "/" + name;
-  }
-
-
-  function ajax(streamer, index){
-    $.ajax({
-      url: prepareURL("streams", streamer),
-      type: 'GET',
-      dataType: 'json',
-      headers: {
-          'Client-ID': clientID,
-      },
-      success: function(data){
-        api_streamers[index] = data;
-      },
-      error: function(error){
-          console.log(error);
-      }
-    })
-  };
-
-  let all = $('#all-users');
-
-  let all_section = $('.online');
-
-  console.log(all_section);
-  all.on('click', function(){
-    showResponse();
-  })
-
-  function showResponse(){
-    api_streamers.forEach(function(streamer){
-      console.log(streamer._links.channel);
-      all_section.append(streamer._links.channel + '\n');
+    streamers.forEach(function(streamer, index){
+        ajax(streamer, index);
     });
-  }
+
+    function prepareURL(type, name){
+        return baseURL + "/" + type + "/" + name;
+    }
+
+    function ajax(streamer, index){
+      $.ajax({
+        url: prepareURL("streams", streamer),
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+          'Client-ID': clientID,
+        },
+        success: function(data){
+          api_streamers[index] = data;
+        },
+        error: function(error){
+          console.log(error);
+        }
+      });
+    };
+
+    let all = $('#all-users');
+    let online = $('#online-users');
+    let offline = $('#offline-users');
+
+    let all_section = $('.online');
+
+    all.on('click', function(){
+        showAll();
+    });
+
+    online.on('click', function(){
+        showOnline();
+    });
+
+    offline.on('click', function(){
+        showOffline();
+    });
+
+    function showAll(){
+        console.log('showall clicked');
+        all_section.empty();
+        api_streamers.forEach(function(streamer){
+            all_section.append(streamer._links.channel + '\n');
+        });
+    }
+
+    function showOnline(){
+        all_section.empty();
+        api_streamers.forEach(function(streamer){
+            if(streamer.stream !== null){
+                all_section.append(streamer._links.channel + '\n');
+            };
+        });
+    }
+
+    function showOffline(){
+        all_section.empty();
+        api_streamers.forEach(function(streamer){
+            if(streamer.stream === null){
+                all_section.append(streamer._links.channel + '\n');
+            };
+        });
+    }
 });
 
