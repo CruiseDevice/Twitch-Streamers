26b6d3114fff9fcd28ac3d71da54fc9e90256f88
Akash Chavan <achavan1211@gmail.com>
2019-08-18 03:07:19 +0530

    Revert "refactoring the complete code."

--- js/app.js
+++ js/app.js
@@ -1,146 +1,120 @@
-$(document).ready(function(){
-    let clientID = config.Client_ID;
-    let $streamers = $(".streamers-list");
-    let streamers = ["ESL_SC2", "ESL_CSGO", "freecodecamp", "storbeck",
-                    "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger",
-                    "noobs2ninjas", "beohoff", "brunofin", "comster404",
-                    "test_channel", "cretetion", "sheevergaming", "TR7K",
-                    "OgamingSC2", "monstercat", "pink_sparkles"];
+// console.log('working');
+var $streamers = $(".streamers-list");
+var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var x = 0;
 
-    let x = 0;
-    let baseURL = "https://api.twitch.tv/kraken";
-    let api_streamers = [];
-
-    var logo = "https://cdn.designbyhumans.com/product_images/p/942272.f56.2e446S7ay1Cm2MjUAAA-650x650-b-p.jpg";
-
-    streamers.forEach(function(streamer, index){
-        ajax(streamer, index);
-    });
-
-    function prepareURL(type, name){
-        return baseURL + "/" + type + "/" + name;
-    }
-
-    function ajax(streamer, index){
-      $.ajax({
-        url: prepareURL("streams", streamer),
-        type: 'GET',
-        dataType: 'json',
-        headers: {
-          'Client-ID': clientID,
-        },
-        success: function(data){
-          api_streamers[index] = data;
-        },
-        error: function(error){
-          console.log(error);
-        }
-      });
-    };
-
-    let all = $('#all-users');
-    let online = $('#online-users');
-    let offline = $('#offline-users');
-
-    let all_section = $('.online');
-
-    all.on('click', function(){
-        showAll();
-    });
-
-    online.on('click', function(){
-        showOnline();
-    });
-
-    offline.on('click', function(){
-        showOffline();
-    });
-
-    function showAll(){
-        console.log('showall clicked');
-        all_section.empty();
-        api_streamers.forEach(function(streamer){
-            console.log(streamer.stream);
-            if(streamer.stream !== null){
-                all_section.append(
-                        '<div class="card">'                                                                            +
-                            '<div class="card-horizontal">'                                                             +
-                                '<div class="img-square-wrapper col-md-4">'                                             +
-                                    "<img class='logo' src="+streamer.stream.channel.logo+">"                           +
-                                '</div>'                                                                                +
-                                '<div class="card-block col-md-5 px-3">'                                                +
-                                    streamer.stream.channel.name                                                        +
-                                    '<br/>'                                                                             +
-                                    '<p>'                                                                               +
-                                        streamer.stream.game + ": " + streamer.stream.channel.status                    +
-                                    '</p>'                                                                              +
-                                '</div>'                                                                                +
-                            '</div>'                                                                                    +  
-                        '</div>'
-                );    
-            }else{
-                all_section.append(
-                        '<div class="card">'                                                                            +
-                            '<div class="card-horizontal">'                                                             +
-                                '<div class="img-square-wrapper col-md-4">'                                             +
-                                    "<img class='logo' src="+logo+">"                                                   + 
-                                '</div>'                                                                                +
-                                '<div class="card-block col-md-5 px-3">'                                                +
-                                    streamer._links.channel.split('/')[5]                                               +
-                                    '<br/>'                                                                             +
-                                    '<p>Offline</p>'                                                                    +
-                                '</div>'                                                                                + 
-                            '</div>'                                                                                    +
-                        '</div>'
-                );
-            }
-        });
-    }
-
-    function showOnline(){
-        all_section.empty();
-        api_streamers.forEach(function(streamer){
-            if(streamer.stream !== null){
-                all_section.append(
-                    '<div class="card">'                                                                                +
-                        '<div class="card-horizontal">'                                                                 + 
-                            '<div class="img-square-wrapper col-md-4">'                                                 +
-                                "<img class='logo' src="+streamer.stream.channel.logo+">"                               +
-                            '</div>'                                                                                    +
-                            '<div class="card-block col-md-5 px-3">'                                                    + 
-                                streamer.stream.channel.name                                                            +
-                                '<br/>'                                                                                 +
-                                '<p>'                                                                                   + 
-                                    streamer.stream.game + ": "+ streamer.stream.channel.status                         +
-                                '</p>'                                                                                  +
-                            '</div>'                                                                                    +
-                        '</div>'                                                                                        + 
-                    '</div'
-                );
-            };
-        });
-    }
-
-    function showOffline(){
-        all_section.empty();
-        api_streamers.forEach(function(streamer){
-            if(streamer.stream === null){
-                all_section.append(
-                    '<div class="card">'                                                                                +
-                        '<div class="card-horizontal">'                                                                 +
-                            '<div class="img-square-wrapper col-md-4">'                                                 +
-                                "<img class='logo' src="+logo+">"                                                       +  
-                            '</div>'                                                                                    +
-                            '<div class="card-block col-md-5 px-3">'                                                    +
-                                streamer._links.channel.split('/')[5]                                                   +
-                                '<br/>'                                                                                 +
-                                '<p>Offline</p>'                                                                        +
-                            '</div>'                                                                                    + 
-                        '</div>'                                                                                        +
-                    '</div>'
-                );
-            };
-        });
-    }
+streamers.forEach(function(streamer){
+  ajax(streamer);
 });
 
+function ajax(streamer){
+  console.log(streamer);
+  url = "https://wind-bow.gomix.me/twitch-api/streams/" + streamer + "?callback=?",
+  console.log(url);
+  $.ajax({
+    url: url,
+    type: 'GET',
+    dataType: 'jsonp',
+    data: {
+      action: 'query',
+      format:'json'
+    }
+  })
+  .done(function(response) {
+    // console.log(url);
+    // console.log(response);
+    showResponse(response);
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+}
+function showResponse(response){
+  console.log(response);
+  if(response.stream === null){
+    url = response._links.channel.substr(38);
+    // console.log(url);
+    updateOfflineUsers();
+  }else if(response.status == 422 || response.status == 404){
+    status = response.message;
+    updateHTML(".unavailable");
+  }else{
+    if(response.stream.channel.logo !== null){
+      picture = 'url("'+response.stream.channel.logo+'")';
+    }else{
+      picture = 'url("https://cdn.rawgit.com/ayoisaiah/freeCodeCamp/master/twitch/images/placeholder-2.jpg")';
+    }
+    url = response._links.channel.substr(38);
+    status = "<a href='https://twitch.tv/" + url + "' target='_blank'" + "'>" + response.stream.channel.display_name +  "</a>" + " is currently streaming " + response.stream.game;
+    updateHTML(".online");
+  }
+}
+function updateOfflineUsers(){
+  $.ajax({
+    url: 'https://wind-bow.gomix.me/twitch-api/channels/'+url,
+    type: 'GET',
+    dataType: 'jsonp',
+    data: {format: 'json'}
+  })
+  .done(function(response) {
+    console.log(response);
+    status = "Channel " + "'<a href='" + response.url + "' target='_blank'" + "'>" + response.display_name + "</a>'" + " is currently offline";
+    if(response.logo !== null){
+      picture = 'url("'+response.logo+'")';
+    }else{
+      picture = 'url("https://cdn.rawgit.com/ayoisaiah/freeCodeCamp/master/twitch/images/placeholder-2.jpg")';
+    }
+    updateHTML(".offline");
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+
+}
+function updateHTML(section){
+  $(section).html(''                                                  +
+  '<div class="twitch">'                                              +
+    '<div class="row">'                                               +
+      '<div class="one-third column">'                                +
+        '<div class="image-holder" id="user-image-' + x + '">'        +
+        '</div>'                                                      +
+      '</div>'                                                        +
+      '<div class="two-thirds column">'                               +
+        '<span class="status-message">' + status + '</span>'          +
+      '</div>'                                                        +
+    '</div>'                                                          +
+  '</div>');
+  if (section == ".online" || section == ".offline") { //If users are online or offline, load profile images
+			$("#user-image-" + x).css({
+				background: picture,
+				'background-size': '55px'
+			});
+		}
+		x++;
+}
+function onlineUsers(){ // display online users
+  $(".offline-users, .all-users").removeClass('focus');
+  $(".online-users").addClass('focus');
+  $(".offline, .unavailable").addClass('hidden');
+  $(".online").removeClass('hidden');
+  console.log('working onlineusers');
+}
+function offlineUsers(){
+  console.log('offline users working');
+  $(".online-users,.all-users").removeClass('focus');
+  $(".offline-users").addClass('focus');
+  $(".online, .unavailable").addClass('hidden');
+  $(".offline").removeClass('hidden');
+}
+$(".online-users").click(function(){
+  onlineUsers();
+});
+$(".offline-users").click(function(){
+  offlineUsers();
+});
